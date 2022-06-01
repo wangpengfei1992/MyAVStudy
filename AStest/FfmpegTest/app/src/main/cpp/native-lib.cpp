f18df475aae8eed59ed89be1e87d4fc244b24c8b
Pengfei Wang <pengfei.wang@anker.com>
2022-06-01 18:24:54 +0800

    FFmpegTest 添加工程配置

--- AStest/FfmpegTest/app/src/main/cpp/native-lib.cpp
+++ AStest/FfmpegTest/app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,41 @@
 #include <jni.h>
 #include <string>
+#include "util/LogUtil.h"
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavfilter/avfilter.h>
+#include <libavformat/avformat.h>
+#include <libavutil/avutil.h>
+#include <libswresample/swresample.h>
+#include <libswscale/swscale.h>
+
+}
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_wpf_ffmpegtest_MainActivity_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+
+/*    std::string hello = "Hello from C++";
+    LOGE("my log hello word");
+    return env->NewStringUTF(hello.c_str());*/
+
+    //获取ffmpeg版本信息
+    char data[1024 * 4] = {0};
+    strcat(data, "libavcodec: ");
+    strcat(data, AV_STRINGIFY(LIBAVCODEC_VERSION));
+    strcat(data, "\nlibavfilter: ");
+    strcat(data, AV_STRINGIFY(LIBAVFILTER_VERSION));
+    strcat(data, "\nlibavformat: ");
+    strcat(data, AV_STRINGIFY(LIBAVFORMAT_VERSION));
+    strcat(data, "\nlibavutil: ");
+    strcat(data, AV_STRINGIFY(LIBAVUTIL_VERSION));
+    strcat(data, "\nlibswresample: ");
+    strcat(data, AV_STRINGIFY(LIBSWRESAMPLE_VERSION));
+    strcat(data, "\nlibswscale: ");
+    strcat(data, AV_STRINGIFY(LIBSWSCALE_VERSION));
+
+
+    return env->NewStringUTF(data);
 }
